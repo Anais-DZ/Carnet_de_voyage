7ee9e51d9371c94d600ce56048d9b128219bb0dd
anais_diez <diez.anais@gmail.com>
2025-02-16 20:59:27 +0100

    ajout avion et nuage animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 anime({
     targets: '#box1',
-    translateX: '-1200', // Se déplace vers la gauche
+    translateX: '-1400', // Se déplace vers la gauche
     //opacity: [1, 0], // Devient invisible à la fin
     duration: 10000, // Plus lent (3s)
     easing: 'linear',
@@ -8,5 +8,31 @@ anime({
     complete: function(anime) {
         // Remet l'élément à droite instantanément et le rend visible
         anime.set('#box1', { translateX: 1200, opacity: 1 });
+    }
+});
+
+anime({
+    targets: '.nuage',
+    translateX: '1500', // Se déplace vers la gauche
+    //opacity: [1, 0], // Devient invisible à la fin
+    duration: 25000, // Plus lent (3s)
+    easing: 'linear',
+    loop: true,
+    complete: function(anime) {
+        // Remet l'élément à droite instantanément et le rend visible
+        anime.set('#box1', { translateX: -1500, opacity: 1 });
+    }
+});
+
+anime({
+    targets: '.nuage2',
+    translateX: '1600', // Se déplace vers la gauche
+    //opacity: [1, 0], // Devient invisible à la fin
+    duration: 25000, // Plus lent (3s)
+    easing: 'linear',
+    loop: true,
+    complete: function(anime) {
+        // Remet l'élément à droite instantanément et le rend visible
+        anime.set('#box1', { translateX: -1600, opacity: 1 });
     }
 });
